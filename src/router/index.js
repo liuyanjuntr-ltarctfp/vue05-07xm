ee217b50f6875b5bcf3c44d315b7a370ed8ebfdf
liuyanjuntr-ltarctfp <3186335192@qq.com>
2020-05-07 11:40:29 +0800

    vue
    Signed-off-by: liuyanjuntr-ltarctfp <3186335192@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import register from '@/views/sh/register'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    mode: 'history',
+    routes: [{
+            path: '/',
+            redirect: '/zc'
+        },
+        {
+            path: '/zc',
+            name: 'register',
+            component: register
+        }
+    ]
 })
